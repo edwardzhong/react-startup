0834e1b7e5fccdd4fe7d1c0187523a0df343ed50
edwardzhong <zhongjfgd@163.com>
2021-08-06 16:44:38 +0800

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  target: 'web',
   devServer: {
     contentBase: './dist',
     port: 4001,
